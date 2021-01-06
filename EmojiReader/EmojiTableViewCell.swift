2edf830a2c2038fb141293f1c1e3b01d21aa3711
Alexander Sobolev <sobolev.ios.developer@gmail.com>
2021-01-06 14:34:00 +0300

    Added new feature

--- EmojiReader/EmojiTableViewCell.swift
+++ EmojiReader/EmojiTableViewCell.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class EmojiTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var EmojiLablel: UILabel!
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var descriptionLable: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
