0271c00e20029d14f102117e99328b8d2097587b
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-01-23 18:26:58 +0300

    Added updateSaveButtonState

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
+    
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavourite: false)
 
     @IBOutlet weak var emojiTextField: UITextField!
     
@@ -20,10 +22,33 @@ class NewEmojiTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        updateSaveButtonState()
         
     }
-    @IBAction func textChange(_ sender: UITextField) {
+
+    private func updateSaveButtonState() {
+        let emojiText = emojiTextField.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descripyionTextField.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
     }
     
+    
+    @IBAction func textChange(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "saveSegue" else { return }
+        
+        let emoji = emojiTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descripyionTextField.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavourite: self.emoji.isFavourite)
+        
+    }
 
 }
